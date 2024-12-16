9903754083ef2e315c3830104de813e888a7cb13
maorgigi123 <maorgigim1509@gmail.com>
2024-12-16 12:03:32 +0200

    a

--- src/App.js
+++ src/App.js
@@ -83,6 +83,7 @@ function App() {
       if (dataExists) {
         // If data exists, don't send a new request
         setLoading(false);
+        setFinishAllPhotos(false);
         return;
       }
 
